1cc73dae58c8c23afc1e2c47cd4f54e685a99ee2
Donald Shan <xxpinapelzxx@gmail.com>
2021-07-20 19:57:35 -0700

    Add files via upload

---
+++ cordova_plugins.js
@@ -0,0 +1,34 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-splashscreen.SplashScreen",
+      "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+      "pluginId": "cordova-plugin-splashscreen",
+      "clobbers": [
+        "navigator.splashscreen"
+      ]
+    },
+    {
+      "id": "cordova-plugin-statusbar.statusbar",
+      "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+      "pluginId": "cordova-plugin-statusbar",
+      "clobbers": [
+        "window.StatusBar"
+      ]
+    },
+    {
+      "id": "monaca-plugin-monaca-core.monaca",
+      "file": "plugins/monaca-plugin-monaca-core/www/monaca.js",
+      "pluginId": "monaca-plugin-monaca-core",
+      "merges": [
+        "monaca"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-splashscreen": "6.0.0",
+    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-plugin-statusbar": "2.4.3",
+    "monaca-plugin-monaca-core": "3.3.1"
+  };
+});
